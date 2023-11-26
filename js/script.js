3c4b3b9448dde3582c4e7eafa7d34dbd2122ec94
lee_c <gitleelee@gmail.com>
2023-11-25 20:46:07 -0800

    Fetch repos, format repo display, and fixed comments.

--- js/script.js
+++ js/script.js
@@ -1,30 +1,48 @@
 // Global variables
 const username = "lee77carter"
 const overview = document.querySelector(".overview");
+const repoList = document.querySelector(".repo-list");
 
-// fetch users data
+// fetch user
 const getUserData = async function () {
     const res = await fetch(`https://api.github.com/users/${username}`);
     const data = await res.json();
-    displayUserInfo(data);
-  };
-  getUserData();    
+    displayUserInfo(data); // display user data
+    getRepos(data);
+};
+getUserData();
 
-
-// user data displayed
+// Fetch user data info
 const displayUserInfo = function (data) {
     const div = document.createElement("div");
     div.classList.add("user-info");
     div.innerHTML = `
-      <figure>
-        <img alt="user avatar" src=${data.avatar_url} />
-      </figure>
-      <div>
-        <p><strong>Name:</strong> ${data.name}</p>
-        <p><strong>Bio:</strong> ${data.bio}</p>
-        <p><strong>Location:</strong> ${data.location}</p>
-        <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-      </div>
-    `;
+        <figure>    
+            <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>
+        `;
     overview.append(div);
-  };
+    getRepos();
+};
+
+// Fetch repos 
+const getRepos = async function () {
+    const showRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await showRepos.json();
+    displayRepos(repoData);
+};
+// Display info about each repo
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+};
